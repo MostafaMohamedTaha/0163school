afa1c124e0eb4bc7ef79ba42f42d2db0b203456f
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2024-12-20 22:56:15 +0200

    0

---
+++ school/SchoolProject.Data/Entities/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Data.Entities
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Students = new HashSet<Student>();
+            Departments = new HashSet<DepartmentSubject>();
+
+        }
+        public int DID { get; set; }
+        [StringLength(500)]
+        public string Name { get; set; }
+        [InverseProperty("Departments")]
+        public virtual ICollection<Student> Students { get; set; }
+        public virtual ICollection<DepartmentSubject> Departments { get; set; }
+    }
+}
---
+++ school/SchoolProject.Data/Entities/DepartmentSubject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Data.Entities
+{
+    public class DepartmentSubject
+    {
+        [Key]
+        public int DeptSubID { get; set; }
+        public int DID { get; set; }
+        public int SubID { get; set; }
+        [ForeignKey("DID")]
+        public virtual Department Departments { get; set; }
+        [ForeignKey("SubID")]
+        public virtual Subjects Subjects { get; set; }
+    }
+}
